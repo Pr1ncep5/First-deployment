9b140c36853dfec5321fb0c2d38cc6f918916a50
Vladimir <volodya.mozgovykh@gmail.com>
2025-08-12 16:55:56 +0200

    Delete all from backend folder

--- backend/convex/greetings.ts
+++
@@ -1,9 +0,0 @@
-import { query } from "./_generated/server";
-import type { QueryCtx } from "./_generated/server";
-
-export const hello = query({
-  args: {},
-  handler: async (_ctx: QueryCtx) => {
-    return "Hello from Convex! This is the first message from the backend!";
-  },
-});

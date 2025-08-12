d2ad466685e514e4cf334a813f3d706f59f14f3f
Vladimir <volodya.mozgovykh@gmail.com>
2025-08-12 19:15:04 +0200

    Forgot to add to the root

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -11,6 +11,9 @@ export const Route = createRootRoute({
         <Link to="/about" className="[&.active]:font-bold">
           About
         </Link>
+        <Link to="/test" className="[&.active]:font-bold">
+          Test
+        </Link>
       </div>
       <hr />
       <Outlet />

1be4bacc925ec1d33deb2d5677c7b89e6c232476
Vladimir <volodya.mozgovykh@gmail.com>
2025-08-12 15:56:05 +0200

    Fix the unused variable

--- backend/convex/greetings.ts
+++ backend/convex/greetings.ts
@@ -2,7 +2,7 @@ import { query } from "./_generated/server";
 
 export const hello = query({
   args: {},
-  handler: async (ctx) => {
+  handler: async (_ctx) => {
     return "Hello from Convex! This is the first message from the backend!";
   },
 });

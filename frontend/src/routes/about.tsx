e35fc243a7124ebdc258b13d539156d8896b8704
Vladimir <volodya.mozgovykh@gmail.com>
2025-08-15 14:59:32 +0200

    add tasks?.

--- frontend/src/routes/about.tsx
+++ frontend/src/routes/about.tsx
@@ -1,7 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import {
-  useQuery
-} from "convex/react";
+import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 
 export const Route = createFileRoute("/about")({
@@ -15,7 +13,7 @@ function About() {
     <div className="p-4 md:p-8 lg:p-12">
       <h3 className="text-3xl">About Page</h3>
       <div>
-        {tasks.map((task: any) => (
+        {tasks?.map((task: any) => (
           <div key={task._id}>
             <p className="text-lg">{task.text}</p>
           </div>

5ce58c37f492014ee54bc58a1fa6c21a72a7fcc8
Vladimir <volodya.mozgovykh@gmail.com>
2025-08-15 15:57:40 +0200

    now?

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useSession } from "../lib/auth-client";
-import { LoginCard } from "../components/login";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -9,17 +8,9 @@ export const Route = createFileRoute("/")({
 function Index() {
   const session = useSession();
 
-  if (session && session.data) {
-    return (
-      <div className="p-2">
-        <p>Hello, dear {session.data.user.name}!</p>
-      </div>
-    );
-  }
-
   return (
-    <div className="flex h-screen items-center justify-center">
-      <LoginCard />
+    <div className="p-2">
+      <p>Hello, dear {session.data?.user?.name}!</p>
     </div>
   );
 }
